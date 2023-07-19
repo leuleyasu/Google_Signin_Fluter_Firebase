b884df66adc6ef899870ec933c4279030b65f1c5
leuleyasu <leuleyasu7@gmail.com>
2023-07-19 14:02:08 +0300

    feat:finishing google signin, signout with fireabse

--- client/lib/feature/Provider/google_signin_provider.dart
+++ client/lib/feature/Provider/google_signin_provider.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +7,7 @@ class GoogleSignProvider extends ChangeNotifier {
 final googleSignIn = GoogleSignIn();
 GoogleSignInAccount? _user;
 GoogleSignInAccount get user => _user!;
-Future googleLogin()async{
+Future Login()async{
 final googleuser= await googleSignIn.signIn();
 if (googleuser==null) return
 _user=googleuser;
